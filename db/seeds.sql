5bd3cd0c897441f7164a30a455fcd7558a1435fa
Michael Le <58507227+michael20996@users.noreply.github.com>
2020-04-01 15:50:05 -0700

    Created and updated database files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUES("BigMac", true);
+INSERT INTO burgers(burger_name, devoured) VALUES("Whopper", false);
+INSERT INTO burgers(burger_name, devoured) VALUES("JumboJack", true);
